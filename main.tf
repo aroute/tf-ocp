04c32c6d949139a97a5e44b826639ada2d059a63
Arif <aroute@users.noreply.github.com>
2021-03-22 07:53:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
-  resource "ibm_container_cluster" "cluster" {
-    name              = "${var.cluster_name}${random_id.name.hex}"
-    datacenter        = var.datacenter
-    default_pool_size = 3
-    machine_type      = var.machine_type
-    hardware          = var.hardware
-    kube_version      = var.kube_version
-    public_vlan_id    = var.public_vlan_id
-    private_vlan_id   = var.private_vlan_id
-  }
+data "ibm_resource_group" "resource_group" {
+  name = "default"
+}
 
+resource "ibm_container_cluster" "tfcluster" {
+    name            = "tfcluster"
+    datacenter      = "dal13"
+    machine_type    = "b3c.4x16"
+    hardware        = "shared"
+    public_vlan_id  = "2910272"
+    private_vlan_id = "2910274"
+
+    kube_version = "4.6_openshift"
+
+    default_pool_size = 3
+
+    public_service_endpoint  = "true"
+    private_service_endpoint = "true"
+
+    resource_group_id = data.ibm_resource_group.resource_group.id
+}
